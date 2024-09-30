f66923654185bb62be88eaa734a11158c5e799b2
Advait Palve <41812619+AdvaitPalve11@users.noreply.github.com>
2024-09-30 12:29:32 +0530

    Add files via upload

---
+++ Dice roll.py
@@ -0,0 +1,19 @@
+import random 
+
+while True: 
+   
+ choice = (input("Roll dice? (Y/N) ")).lower()
+   
+ if choice == "y":
+    num1 = random.randint(1,6)
+    num2 = random.randint(1,6)
+    print(f"({num1},{num2})")
+   
+ elif choice == "n":
+    print("Thanks for Playing!")
+    break
+
+ else : 
+    print("Invalid Input")
+    
+
